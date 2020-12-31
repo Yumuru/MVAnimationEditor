1930c27f52bc6f6f91cd17476382191fe3137f46
Yumuru <yakasa30@yahoo.co.jp>
2020-12-31 23:53:42 +0900

    Delete Process Action

--- Assets/Scripts/Editor/AnimationGraph/NodeConstructor.cs
+++ Assets/Scripts/Editor/AnimationGraph/NodeConstructor.cs
@@ -14,7 +14,6 @@ public interface IProcessNode : IAnimationGraphNode {
 public class ProcessPort {
   public Port InputPort;
   public Port OutputPort;
-  public Action execute { get; set; }
   public Port CreateInput() { 
     InputPort = Port.Create<Edge>(Orientation.Horizontal, Direction.Input, Port.Capacity.Single, typeof(ProcessPort));
     InputPort.portName = "In";
@@ -24,13 +23,6 @@ public class ProcessPort {
     OutputPort = Port.Create<Edge>(Orientation.Horizontal, Direction.Output, Port.Capacity.Single, typeof(ProcessPort));
     OutputPort.portName = "Out";
     return OutputPort;
-  }
-  public void ProcessExecute(Node node) {
-    execute?.Invoke();
-    foreach (var edge in OutputPort.connections) {
-      var nextNode = edge.output.node;
-      ((IProcessNode) nextNode).processPort.ProcessExecute(nextNode);
-    }
   }
 }
 }
